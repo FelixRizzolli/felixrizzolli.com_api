4a9c92c9107cada799cfd4ab1c761d76aa75eac3
Felix Rizzolli <felix.rizzolli@outlook.de>
2025-10-19 08:44:50 +0000

    feat(payloadcms): update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,32 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
+import redirects from './redirects.js'
+
+const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  images: {
+    remotePatterns: [
+      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+        const url = new URL(item)
+
+        return {
+          hostname: url.hostname,
+          protocol: url.protocol.replace(':', ''),
+        }
+      }),
+      {
+        protocol: 'http',
+        hostname: '*.next',
+        port: '3000',
+      },
+    ],
+  },
+
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
@@ -11,6 +35,35 @@ const nextConfig = {
     }
 
     return webpackConfig
+  },
+
+  reactStrictMode: true,
+  redirects,
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+
+      {
+        source: '/admin/:path*',
+        destination: '/admin/:path*',
+      },
+
+      {
+        source: '/:path*',
+        destination: '/:tenant/:path*',
+
+        has: [
+          {
+            type: 'host',
+            value: '(?<tenant>.*)',
+          },
+        ],
+      },
+    ]
   },
 }
 
